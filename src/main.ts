cf5c53ef63498fd3f642bd4fad69ec7d8df4d1a3
ruan-cat <1219043956@qq.com>
2025-10-09 11:36:28 +0800

    🐳 chore(root): 说明 global-ku-root 手动注册方案属于临时方案。标记待办。

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ export function createApp() {
   app.use(requestInterceptor)
   app.use(VueQueryPlugin)
 
+  // TODO: 经过沟通，此处的手动注册组件属于临时方案 对应的依赖升级并发版后 就可以手动移除了
   app.component('global-ku-root', GlobalKuRoot)
 
   return {
