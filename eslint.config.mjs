07909830ee8d3c05b8f9c0f7ff5b28a41e3884f6
ruan-cat <1219043956@qq.com>
2025-12-05 10:13:33 +0800

    🔧 config(config)!: 现在 gitee-example 文件夹中的所有文件都不会被 ESLint 检查了

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,8 @@ export default uniHelper(
     ignores: [
       'src/uni_modules/',
       'dist',
+      // 忽略旧版 Vue2 示例代码
+      'gitee-example/',
       // unplugin-auto-import 生成的类型文件，每次提交都改变，所以加入这里吧，与 .gitignore 配合使用
       'auto-import.d.ts',
       // vite-plugin-uni-pages 生成的类型文件，每次切换分支都一堆不同的，所以直接 .gitignore
